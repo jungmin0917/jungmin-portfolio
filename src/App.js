35952e34fd183e06d3fde65af26660ea6ce2ca7e
jungmin0917 <jungmin09172@naver.com>
2023-12-19 15:40:34 +0900

    포트폴리오 사이트 제작 시작 (레퍼런스: https://webstoryboy.co.kr/1934)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      return (
+        <h1>황정민 포트폴리오</h1>
+        <div className={"portfolio-link"}>
+          <a href="https://knotted.jungmin.site/">노티드</a>
+          <a href="https://kurly.jungmin.site/">컬리</a>
+        </div>
+      );
     </div>
   );
-}
+};
 
 export default App;
