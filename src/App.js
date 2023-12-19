7bd2c6f04d817ef0c9c55fdb84c922b32588f2da
jungmin0917 <jungmin09172@naver.com>
2023-12-19 16:27:08 +0900

    개인 포트폴리오 사이트 - 리액트 기본 디렉터리 구조 작성

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from 'react';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HomeView from "./pages/HomeView";
 
 const App = () => {
   return (
-    <div>
-        <h1>황정민 포트폴리오</h1>
-        <div className={"portfolio-link"}>
-          <a href="https://knotted.jungmin.site/">노티드</a>
-          <a href="https://kurly.jungmin.site/">컬리</a>
-        </div>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={"/"} element={<HomeView/>} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
