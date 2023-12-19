8e8f0caab41f39b1951c7685835cdccd2fe73bd4
jungmin0917 <jungmin09172@naver.com>
2023-12-19 16:23:49 +0900

    개인 포트폴리오 사이트 - 리액트 기본 디렉터리 구조 작성

---
+++ src/views/HomeView.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from "../components/Main";
+
+const HomeView = () => {
+  return (
+    <>
+      <Main>
+
+      </Main>
+    </>
+  );
+};
+
+export default HomeView;
