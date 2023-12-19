39dfa9fda25f2b17ca38d93ccac4e1c5a903d686
jungmin0917 <jungmin09172@naver.com>
2023-12-19 12:43:58 +0900

    CI/CD 파이프라인 구축 중

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,24 @@
 #!/usr/bin/env bash
 
-echo "> 리액트 애플리케이션은 정적 파일이라 별도의 실행이 필요하지 않음"
+REPOSITORY=/home/ubuntu/app/jungmin
+PROJECT_NAME=jungmin
+
+echo "> 현재 구동 중인 애플리케이션 pid 확인"
+
+CURRENT_PID=$(pgrep -fl $PROJECT_NAME | grep node | awk '{print $1}')
+
+echo "현재 구동 중인 애플리케이션 pid: $CURRENT_PID"
+
+if [ -z "$CURRENT_PID" ]; then
+  echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
+  echo "> kill -15 $CURRENT_PID"
+  kill -15 $CURRENT_PID
+  sleep 5
+fi
+
+echo "> 새 애플리케이션 배포"
+
+# npm start 실행
+export PORT=3000
+nohup npm start > $REPOSITORY/nohup.out 2>&1 &
