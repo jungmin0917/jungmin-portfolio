26c9ef79491a379226a52823f7ca5d560dbd4082
jungmin0917 <jungmin09172@naver.com>
2023-12-19 13:54:17 +0900

    CI/CD 파이프라인 구축 중

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,14 @@
 #!/usr/bin/env bash
 
-echo "> 리액트 애플리케이션은 정적 파일이라 별도의 실행이 필요하지 않음"
+echo "> 리액트 애플리케이션 배포"
+
+# 빌드된 정적 파일의 디렉토리 경로
+BUILD_DIR="/home/ubuntu/app/jungmin/build"
+
+# Nginx가 서빙하는 디렉토리 경로
+NGINX_DIR="/var/www/html"
+
+# 정적 파일 복사
+cp -r "$BUILD_DIR"/* "$NGINX_DIR"
+
+echo "> 리액트 애플리케이션 정적 파일을 nginx 디렉터리로 복사 완료"
