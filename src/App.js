2ddeb03657c35a1caff7201149d18721eaf91f92
jungmin0917 <jungmin09172@naver.com>
2023-12-19 15:43:50 +0900

    포트폴리오 사이트 제작 시작 (레퍼런스: https://webstoryboy.co.kr/1934)

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import React from 'react';
 const App = () => {
   return (
     <div>
-      return (
         <h1>황정민 포트폴리오</h1>
         <div className={"portfolio-link"}>
           <a href="https://knotted.jungmin.site/">노티드</a>
           <a href="https://kurly.jungmin.site/">컬리</a>
         </div>
-      );
     </div>
   );
 };
